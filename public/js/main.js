1d5971d4ca7bcb0f6cc72f1efd92050ec497165c
Jordi Domènech <jdomenech@drivania.com>
2019-08-09 08:38:29 +0200

    Moved online JS to file

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+let eventListScroll = null;
+
+$(document).ready(function () {
+    // Refresh page every 5 minutes
+    setTimeout(() => { location.reload() }, 60 * 5 * 1000);
+
+    updateClock();
+    eventListScrollToTop();
+
+    $('#btn-refresh').click(() => {
+        location.reload();
+    });
+
+    // Go to top of event list after someone scrolled
+    $('.next').scroll(e => {
+        if (eventListScroll) {
+            clearTimeout(eventListScroll);
+        }
+
+        eventListScroll = setTimeout(eventListScrollToTop, 15000)
+    });
+});
+
+function updateClock()
+{
+    let now = new Date();
+
+    $('.clock').html(moment().format('HH:mm'));
+
+    setTimeout(updateClock, 60 * 1000);
+}
+
+function eventListScrollToTop() {
+    $('.next').animate({ scrollTop: 0 });
+}
